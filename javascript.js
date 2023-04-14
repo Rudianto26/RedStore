80b1f8933b14a7ada92448d250d43d9fa5388f6a
Rudianto <arudi0636@gmail.com>
2023-04-14 22:26:22 +0700

    menambahkan single product page

--- javascript.js
+++ javascript.js
@@ -7,3 +7,19 @@ function menutoggle() {
     menuItems.style.maxHeight = '0px';
   }
 }
+
+const productImg = document.getElementById('product-img');
+const smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function () {
+  productImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  productImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  productImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  productImg.src = smallImg[3].src;
+};
